cb8a68a90fc819b5432b3df04a17f90eacc7d85d
Idan <idanah24@gmail.com>
2020-10-06 19:46:02 +0300

    MVC framework, Basic UI, User Login/Register.

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+//Url Root
+define('URLROOT', 'http://localhost/amp-blog');
+//Site name
+define('SITENAME', 'AMP-Blog');
+
+//  App version
+define('APPVERSION', '1.0.0');
+
+//DB Params
+
+define('DB_NAME', 'amp-blog');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'ksc8BF3ZEV5MaeJG');
---
+++ app/views/pages/index.php
@@ -0,0 +1,14 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<div class="jumbotron jumbotron-flud text-center">
+    <div class="container">
+        <h1 class="display-3"><?php echo $data['title']; ?></h1>
+        <p class="lead">Simple blog platform built on a self-implemented MVC framework
+            using Apache-MySQL-PHP
+        </p>
+    </div>
+</div>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
+
+
